1409a8debeb2d3b54ea6ec0fa6d2c10e9ffcefd6
Sang Le <sang.le@agest.vn>
2024-12-11 13:48:04 +0700

    update

--- resources/views/manager/body/footer.blade.php
+++ resources/views/manager/body/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="bg-dark text-white text-center py-3 fixed-bottom">
-    <p class="mb-0">© 2024 Agest. All rights reserved.</p>
+<footer class="bg-dark text-white text-center py-1 fixed-bottom">
+    <p class="mb-0">©2024 Agest. All rights reserved.</p>
 </footer>
